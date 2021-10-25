b35f2753e2b1b15ed1f583d7e128f01564dbd5d1
Vector <hiuon.x@gmail.com>
2021-10-25 17:16:27 +0300

    feat: calculate Hurst-parameter (with errors, fix in future)

--- back/Hurst.go
+++ back/Hurst.go
@@ -2,7 +2,7 @@ package main
 
 import "math"
 
-func getHRS(ds []int, time int){
+func getHRS(ds []int, time int, data *[4]float64, hurstdisp *[4]float64, item int){
 	// Get max and min values in each interval
 	// Вычисляем размах для каждого интервала
 	statR := make([]int, len(ds)/time)
@@ -11,7 +11,7 @@ func getHRS(ds []int, time int){
 	for i := 0; i < len(ds)/time; i++ {
 		mean := 0.0
 		max := 0
-		min := 0
+		min := ds[i]
 		// Математическое ожидание для данного интервала
 		for j := i*time; j < (i + 1) * time; j++ {
 			mean += float64(ds[j])
@@ -35,6 +35,23 @@ func getHRS(ds []int, time int){
 		statS[i] = math.Pow(float64(time), 0.5) * disp
 		statH[i] = math.Log(float64(statR[i])/statS[i]) / math.Log(float64(time) * 0.5)
 	}
+
+	mean := 0.0
+	disp := 0.0
+	for i := 0; i < len(statH); i++ {
+		mean += statH[i]
+	}
+	mean /= float64(len(statH))
+
+	for i := 0; i < len(statH); i++ {
+		disp += math.Pow(mean - statH[i], 2)
+	}
+	disp /= float64(len(statH))
+
+	data[item] = mean
+	hurstdisp[item] = disp
 }
+
+
 
 
--- back/main.go
+++ back/main.go
@@ -1,7 +1,13 @@
 package main
 
+import "fmt"
+
 func main() {
 	stats := [240]dataStats{}
+	timings := [4]int{5, 15, 60, 120}
+	HurstParam := [4]float64{}
+	HurstDisp := [4]float64{}
+	var testData []int
 	for i := 0; i < 240; i++ {
 		stats[i].srcPort = make(map[int]int)
 		stats[i].dstPort = make(map[int]int)
@@ -10,6 +16,15 @@ func main() {
 		stats[i].dstAddrIp = make(map[int]int)
 	}
 	startGetStatistics(&stats)
+	testData = make([]int, 240)
+	for i := 0; i < 240; i++ {
+		testData[i] = stats[i].protocols["IPv4"]
+	}
+	for i := 0; i < len(timings); i++ {
+		getHRS(testData, timings[i], &HurstParam, &HurstDisp, i)
+	}
+
+	fmt.Println(HurstParam)
 }
 
 
